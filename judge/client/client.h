400ad461def6ca94b1fd79808957e2a559ea8462
whusnoopy <whusnoopy@33cb119d-1444-0410-9cdd-430a31b3ebcd>
2008-03-04 07:02:31 +0000

    Add the source files in judge client without finish them

---
+++ judge/client/client.h
@@ -0,0 +1,34 @@
+// Copyright 2008 Flood Team of Wuhan Univ.
+// Author: yewen@mail.whu.edu.cn (Wen, YE)
+
+#ifndef _FLOOD_JUDGE_CLIENT_CLIENT_H__
+#define _FLOOD_JUDGE_CLIENT_CLIENT_H__
+
+enum COMMUNICATION_STATUS {
+	FIRST,
+  ACCEPT = 0,             // 0. Correct answer
+  PRESENTATION_ERROR,     // 1. Presentation error
+  TIME_LIMIT_EXCEED,      // 2. Time limit exceed
+  MEMORY_LIMIT_EXCEED,    // 3. Memory limit exceed
+  WRONG_ANSWER,           // 4. Wrong answer
+  OUTPUT_LIMIT_EXCEED,    // 5. Output limit exceed
+  COMPILE_ERROR,          // 6. Compile error
+  RUNTIME_ERROR_SIGSEGV,  // 7. Runtime-error when buffer overflow, stack
+                          //    overflow or illegal file access
+  RUNTIME_ERROR_SIGFPE,   // 8. Runtime-error when divided by 0
+  RUNTIME_ERROR_SIGBUG,   // 9. Runtime-error when hardware error occurred
+  RUNTIME_ERROR_SIGABRT,  // 10. Runtime-error when programme aborted before it
+                          //     should be finished
+  RUNTIME_ERROR_JAVA,     // 11. Runtime-error of JAVA when there is outputable
+                          //     info
+  RUNTIME_ERROR_PAS,      // 12. Runtime-error of PASCAL when there is
+                          //     outputable info
+  RESTRICTED_FUNCTION,    // 13. Restricted functions, like system call
+  SYSTEM_ERROR,           // 14. Unknown system errors
+  PENDING,                // 15. Pending for Judge
+  COMPILING,              // 16. Judge is compiling the source file
+  RUNNING,                // 17. Judge is running the submit executable file
+  JUDGING                 // 18. Judge is compare the submit result and the
+                          //     standard output
+}
+#endif
---
+++ judge/client/judge.h
@@ -0,0 +1,3 @@
+// Copyright 2008 Flood Team of Wuhan Univ.
+// Author: yewen@mail.whu.edu.cn (Wen, YE)
+
