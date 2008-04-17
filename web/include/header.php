b395ec9b26770afea00281006934c79ea16ecb18
whusnoopy <whusnoopy@33cb119d-1444-0410-9cdd-430a31b3ebcd>
2008-04-17 07:43:16 +0000

    move workspace

--- web/include/header.php
+++ web/include/header.php
@@ -1,52 +1,44 @@
 <?php
-session_start();
+  session_start();
 ?>
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>HomePage of Wuhan Univ. Online Judge</title>
-  <link href="../style/noah.css" rel="stylesheet" type="text/css" />
+  <title>Flood, the Online Judge v3 of Wuhan Univ.</title>
+  <link href="/flood/style/flood.css" rel="stylesheet" type="text/css" />
 </head>
 <?php
-	if (isset($_SESSION['user_id']))
-		$user_id = $_SESSION['user_id'];
-	else
-		$user_id = '';
+  if (isset($_SESSION['user_id']))
+    $user_id = $_SESSION['user_id'];
+  else
+    $user_id = '';
 
-	$hasNewMail = check_new_mail($user_id);
+  $hasNewMail = check_new_mail($user_id);
 ?>
 <body>
 <center>
-	<div id="bar">
-    <a href="../index.php">Home</a>&nbsp;|&nbsp;
-    <a href="../problem/problemList.php">Problems</a>&nbsp;|&nbsp;
-    <a href="../contest/contestList.php">Contests</a>&nbsp;|&nbsp;
-	<a href="../contest/vcontestList.php">Virtual Contests</a>&nbsp;|&nbsp;
-    <a href="../submit/submit.php">Submit</a>&nbsp;|&nbsp;
-    <a href="../status/status.php">Status</a>&nbsp;|&nbsp;
-    <a href="../ranklist/ranklist.php">Ranklist</a>&nbsp;|&nbsp;
-    <a href="../discuss/discussList.php">Discuss</a>&nbsp;|&nbsp;
-    <a href="../user/user.php">User</a>&nbsp;|&nbsp;
-    <a href="../mail/mailList.php">
-      <?php
-        if ($hasNewMail==true)
-          echo "<font color=\"red\">Mail</font></a>";
-        else
-          echo "Mail</a>&nbsp;|&nbsp;";
-	  ?>
-     &nbsp;|&nbsp;
-    <a href="../faq.html" target="_blank">FAQ</a>
-	</div>
-
-
+  <div id="bar">
+    <a href="/flood/index.php">Home</a>&nbsp;|&nbsp;
+    <a href="/flood/problem/problemList.php">Problems</a>&nbsp;|&nbsp;
+    <a href="/flood/contest/contestList.php">Contests</a>&nbsp;|&nbsp;
+    <a href="/flood/contest/vcontestList.php">Virtual Contests</a>&nbsp;|&nbsp;
+    <a href="/flood/submit/submit.php">Submit</a>&nbsp;|&nbsp;
+    <a href="/flood/status/status.php">Status</a>&nbsp;|&nbsp;
+    <a href="/flood/ranklist/ranklist.php">Ranklist</a>&nbsp;|&nbsp;
+    <a href="/flood/discuss/discussList.php">Discuss</a>&nbsp;|&nbsp;
+    <a href="/flood/user/user.php">User</a>&nbsp;|&nbsp;
+    <a href="/flood/mail/mailList.php">
+<?php if ($hasNewMail==true) ?>
+    <font color="red">Mail</font>";
+<?php else ?>
+    "Mail";</a>&nbsp;|&nbsp;
+    <a href="/flood/faq.html" target="_blank">FAQ</a>
+  </div>
 <?php
-
-function check_new_mail($user_id)
-{
-	if($user_id)
-	    return true;
-	else
-		return false;
+function check_new_mail($user_id) {
+  if($user_id)
+    return true;
+  else
+    return false;
 }
-
 ?>
