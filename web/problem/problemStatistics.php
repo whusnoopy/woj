fd30d39c98b9232825d5899ad474f99d3c63e466
felix021 <felix021@33cb119d-1444-0410-9cdd-430a31b3ebcd>
2008-09-18 14:03:50 +0000

    spell fault

--- web/problem/problemStatistics.php
+++ web/problem/problemStatistics.php
@@ -9,5 +9,5 @@ if($problem_id < 1001)die("N/A|N/A");
 $query = "SELECT `accepted`, `submit` FROM problems WHERE problem_id={$problem_id}";
 $result = $conn->query($query);
 $ans = $result->fetch_row();
-die($ans[0]."|".$ans[1]);
+die($ans[1]."|".$ans[0]);
 ?>
