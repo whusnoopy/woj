4607e6f0791d9b87d262a9b9ee3e232d62e9e146
whusnoopy <whusnoopy@33cb119d-1444-0410-9cdd-430a31b3ebcd>
2008-12-06 09:07:56 +0000

    Modify the makefile scripts to make the compile and install process more simply

--- server/startOnlineJudgeServer.sh
+++ server/startOnlineJudgeServer.sh
@@ -1,1 +1,7 @@
-/home/flood/floodprj/server/bin/main --root_dir="/home/flood/worktemp" --logtostderr=false --log_level=4 --configure_path="/home/flood/floodprj/server/bin/configure.xml"
+configure_path="/home/flood/worktemp/bin/configure.xml"
+root_dir="/home/flood/worktemp"
+
+cmd="screen /home/flood/floodprj/server/bin/oj_server --root_dir=\"$root_dir\" --logtostderr=false --log_level=4 --configure_path=\"$configure_path\"
+echo $cmd
+echo "After the server start up, press Ctrl_a then press d to exit screen monitor"
+eval $cmd
